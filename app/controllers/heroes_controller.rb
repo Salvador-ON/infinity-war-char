8305f6fc75e47e54a45734428a35342414d369b4
Salvador <salvador.on@outlook.com>
2020-07-23 20:56:32 -0300

    building heroes controller-unfinished

---
+++ app/controllers/heroes_controller.rb
@@ -0,0 +1,43 @@
+class HeroesController < ApplicationController
+  def index
+
+    getApi()
+  end
+
+  require 'open-uri'
+require 'json'
+
+
+require 'digest'
+
+ def getApi()
+ time = Time.now.to_i
+  publicKey = "18dbfa75016d2c2843fa6814b0130ad6"
+  privateKey = "0214487c8eb1e8077a5b7da3fc2b3ce435e5b69d"
+   value = (time.to_s + privateKey + publicKey).to_s
+
+  md5 = Digest::MD5.new 
+  md5.update "#{value}"
+   
+    
+    url = "https://gateway.marvel.com:443/v1/public/characters?events=29&orderBy=name&limit=100&ts=#{time}&apikey=18dbfa75016d2c2843fa6814b0130ad6&hash="+ md5.to_s
+
+  req = open(url)
+  response_body = JSON.parse(req.read) 
+  sp_array = Array.new()
+  response_body['data']['results'].each do |sup|
+    heroe = { 'name' => sup['name'],
+              'url_image' => sup['thumbnail']['path'] + '.' + sup['thumbnail']['extension'],
+              'urlink' => sup['urls'][0]['url'],'events' => sup['events']['items'].collect {|e| e['name']}
+   }
+    sp_array.push(heroe)
+  end
+
+  puts sp_array
+  puts "---------"
+  puts sp_array[0]['events']
+
+ end
+#29
+ 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'heroes/index'
   get 'spa/index'
   resources :filters,  only: [:index, :update]
   delete :logout, to: "sessions#destroy"
