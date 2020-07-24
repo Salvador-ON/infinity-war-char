f2ba232416bef309030e9675bdc68e43f3a7a249
Salvador <salvador.on@outlook.com>
2020-07-24 16:23:44 -0300

    change fetch method using net http

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -2,15 +2,11 @@ class HeroesController < ApplicationController
   include CurrentUserConcern
   before_action :authorize
   include FetchApiConcern
-  
 
   def index
-    heroes_data = get_api_heroes()
+    heroes_data = fetch_api_heroes
     render json: {
       heroes: heroes_data
     }
   end
-
-  
- 
 end
